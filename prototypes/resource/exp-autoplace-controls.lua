6d8f889aa766a2c8664beb1c581bef077540bdb2
Hutch <cartmen180@gmail.com>
2014-10-19 17:14:25 +0200

    changes to order

--- prototypes/resource/exp-autoplace-controls.lua
+++ prototypes/resource/exp-autoplace-controls.lua
@@ -4,91 +4,91 @@ data:extend(
     type = "autoplace-control",
     name = "ci-copper",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-iron",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-stone",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-coal",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-lead",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-tin",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-quartz",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-gold",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-silver",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-nickel",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-zinc",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-salt",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-titanium",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-tungsten",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
   {
     type = "autoplace-control",
     name = "ci-limestone",
     richness = true,
-    order = "a"
+    order = "z-z"
   },
 }
 )
